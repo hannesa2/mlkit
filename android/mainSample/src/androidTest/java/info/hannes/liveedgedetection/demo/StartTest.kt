5eca362a37de2051b9a092071b1bfb66dda89261
Hannes Achleitner <hannes.software@gmx.at>
2021-03-06 09:23:27 +0100

    Espresso test

---
+++ android/mainSample/src/androidTest/java/info/hannes/liveedgedetection/demo/StartTest.kt
@@ -0,0 +1,32 @@
+package info.hannes.liveedgedetection.demo
+
+import androidx.test.core.graphics.writeToTestStorage
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.screenshot.captureToBitmap
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.mlkit.MLMainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestName
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class StartTest {
+
+    @get:Rule
+    val activityScenarioRule = activityScenarioRule<MLMainActivity>()
+
+    // a handy JUnit rule that stores the method name, so it can be used to generate unique screenshot files per test method
+    @get:Rule
+    var nameRule = TestName()
+
+    @Test
+    fun smokeTestSimplyStart() {
+        Thread.sleep(1000)
+        Espresso.onView(ViewMatchers.isRoot())
+            .captureToBitmap()
+            .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}")
+    }
+}
