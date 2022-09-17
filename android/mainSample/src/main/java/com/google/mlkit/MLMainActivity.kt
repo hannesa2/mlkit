cebe8a1bca0249a30113f9938ba129ef4982f444
Hannes Achleitner <hannes.software@gmx.at>
2022-09-17 20:13:46 +0200

    Remove pointless nonnull

--- android/mainSample/src/main/java/com/google/mlkit/MLMainActivity.kt
+++ android/mainSample/src/main/java/com/google/mlkit/MLMainActivity.kt
@@ -3,10 +3,9 @@ package com.google.mlkit
 import android.os.Bundle
 import android.widget.Toast
 import info.hannes.github.AppUpdateHelper
-import javax.annotation.Nonnull
 
 class MLMainActivity : NavigationActivity() {
-    override fun onCreate(@Nonnull savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ml_main)
         supportFragmentManager
