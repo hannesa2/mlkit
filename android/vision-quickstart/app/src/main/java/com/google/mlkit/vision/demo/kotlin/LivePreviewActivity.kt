adf72375cdadf0e41c96be8eb3e3fce5d44ab559
Hannes Achleitner <hannes.software@gmx.at>
2022-10-02 23:15:49 +0200

    ktLint fixes

--- android/vision-quickstart/app/src/main/java/com/google/mlkit/vision/demo/kotlin/LivePreviewActivity.kt
+++ android/vision-quickstart/app/src/main/java/com/google/mlkit/vision/demo/kotlin/LivePreviewActivity.kt
@@ -98,7 +98,7 @@ class LivePreviewActivity :
     options.add(TEXT_RECOGNITION_DEVANAGARI)
     options.add(TEXT_RECOGNITION_JAPANESE)
     options.add(TEXT_RECOGNITION_KOREAN)
-    options.add(FACE_MESH_DETECTION);
+    options.add(FACE_MESH_DETECTION)
 
     // Creating adapter for spinner
     val dataAdapter = ArrayAdapter(this, R.layout.spinner_style, options)
@@ -279,7 +279,7 @@ class LivePreviewActivity :
           cameraSource!!.setMachineLearningFrameProcessor(SegmenterProcessor(this))
         }
         FACE_MESH_DETECTION -> {
-          cameraSource!!.setMachineLearningFrameProcessor(FaceMeshDetectorProcessor (this));
+          cameraSource!!.setMachineLearningFrameProcessor(FaceMeshDetectorProcessor(this))
         }
         else -> Log.e(TAG, "Unknown model: $model")
       }
@@ -353,7 +353,7 @@ class LivePreviewActivity :
     private const val CUSTOM_AUTOML_LABELING = "Custom AutoML Image Labeling (Flower)"
     private const val POSE_DETECTION = "Pose Detection"
     private const val SELFIE_SEGMENTATION = "Selfie Segmentation"
-    private const val FACE_MESH_DETECTION = "Face Mesh Detection (Beta)";
+    private const val FACE_MESH_DETECTION = "Face Mesh Detection (Beta)"
 
     private const val TAG = "LivePreviewActivity"
   }
